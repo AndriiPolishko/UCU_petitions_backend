af29d892cc75ea76e201f87b05e87b4044c69439
rwmutel <romanmutel@gmail.com>
2023-05-23 14:36:37 +0300

    done beta version of google oauth

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const dotenv = require('dotenv').config();
 const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const cors = require('cors');
+const passport = require('passport');
+const session = require('express-session');
 
 connectDB();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.BACKEND_PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -17,4 +19,52 @@ app.use('/api/petitions', require('./routes/petitionRoutes'));
 
 app.listen(port, () => console.log('Started'));
 
-app.use(errorHandler);
+// app.use(errorHandler);
+
+/*  PASSPORT SETUP  */
+/* more at https://www.loginradius.com/blog/engineering/google-authentication-with-nodejs-and-passportjs/ */
+
+app.use(session({
+  resave: false,
+  saveUninitialized: true,
+  secret: 'SECRET' 
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+/*  Google AUTH  */
+ 
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: `${process.env.BACKEND_URL}:${process.env.BACKEND_PORT}/auth/google/callback`
+  },
+  function(accessToken, refreshToken, profile, done) {
+      console.log(`USER LOGGED IN: ${profile.displayName}, ${profile.emails[0].value}`);      
+      return done(null, profile);
+  }
+));
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] })
+  );
+ 
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    console.log(req.user);
+    const user = req.user;
+    res.redirect(`${process.env.FRONTEND_URL}:${process.env.FRONTEND_PORT}/auth?id=${user.id}&name=${user.displayName}&email=${user.emails[0].value}&photo=${(user.photos && user.photos[0].value) || "https://icon-library.com/images/default-profile-icon/default-profile-icon-24.jpg"}`);
+    res.redirect(`${process.env.FRONTEND_URL}:${process.env.FRONTEND_PORT}/auth`);
+  });
